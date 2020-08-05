c5f31f55d3616e3fee51e0d3dc1138553405a72a
wei_yao <wei.yaoisnull@gmail.com>
2020-08-05 18:03:52 +0800

    add icon to tabbar nav

--- src/components/TabMenu.vue
+++ src/components/TabMenu.vue
@@ -1,23 +1,47 @@
 <template>
   <van-tabbar v-model="active" fixed route>
-    <van-tabbar-item icon="home-o" replace to="/main/expire">过期卡汇总</van-tabbar-item>
-    <van-tabbar-item icon="search" replace to="/main/publication">发行汇总</van-tabbar-item>
-    <van-tabbar-item icon="search" class="animate__animated animate__heartBeat" replace to="/main">主页</van-tabbar-item>
-    <van-tabbar-item icon="friends-o" replace to="/main/company">单位卡汇总</van-tabbar-item>
-    <van-tabbar-item icon="setting-o" replace to="/main/range">卡余额汇总</van-tabbar-item>
+    <van-tabbar-item replace to="/main/movie">
+      <span>首页</span>
+      <template #icon="props">
+        <img :src="props.active ? icon.active[0] : icon.inactive[0]" />
+      </template>
+    </van-tabbar-item>
+    <van-tabbar-item to="/main/movie1">
+      <span>影院</span>
+      <template #icon="props">
+        <img :src="props.active ? icon.active[1] : icon.inactive[1]" />
+      </template></van-tabbar-item>
+    <van-tabbar-item class="animate__animated animate__heartBeat" to="/main/movie2">
+      <span>扫码</span>
+      <template #icon="props">
+        <img :src="props.active ? icon.active[2] : icon.inactive[2]" />
+      </template></van-tabbar-item>
+    <van-tabbar-item to="/main/movie3">
+      <span>我的</span>
+      <template #icon="props">
+        <img :src="props.active ? icon.active[3] : icon.inactive[3]" />
+      </template></van-tabbar-item>
   </van-tabbar>
 </template>
 <script>
-// import { Tabbar, TabbarItem } from 'vant';
 export default ({
   name: 'TabFootMenu',
-  // components: {
-  //   [Tabbar.name]: Tabbar,
-  //   [TabbarItem.name]: TabbarItem
-  // },
   data() {
     return {
-      active: 0
+      active: 0,
+      icon: {
+        inactive: [
+          require("@assets/images/tab-bar-icon/home.png"),
+          require("@assets/images/tab-bar-icon/category.png"),
+          require("@assets/images/tab-bar-icon/scan.png"),
+          require("@assets/images/tab-bar-icon/me.png")
+        ],
+        active: [require("@assets/images/tab-bar-icon/home_active.png"),
+          require("@assets/images/tab-bar-icon/category_active.png"),
+          require("@assets/images/tab-bar-icon/scan.png"),
+          require("@assets/images/tab-bar-icon/me_active.png")
+        ]
+      }
     }
   }
 })
